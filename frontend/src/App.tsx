c0f402db1c397e52b0450d72ed788157b69d8468
RobertGardner13 <robbygardner26@gmail.com>
2025-04-07 14:31:38 -0600

    routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,24 @@
 import { Routes, Route } from 'react-router-dom';
-
+import CreateAccount from './pages/CreateAccount';
+import Login from './pages/Login';
+import Movie from './pages/Movie';
+import Privacy from './pages/Privacy';
+import ProductDetail from './pages/ProductDetail';
+import Home from './pages/Home';
 
 function App() {
-  
-
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} /> 
         <Route path="/createAccount" element={<CreateAccount />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/movies" element={<Movie />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/productDetail" element={<ProductDetail />} />
-        <Route path="/createAccount" element={<CreateAccount />} />
       </Routes>
-
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/pages/CreateAccount.tsx
@@ -0,0 +1,9 @@
+function CreateAccount() {
+    return (
+        <div>
+            <h1>Welcome to the CreateAccount page</h1>
+        </div>
+    )
+}
+
+export default CreateAccount;
