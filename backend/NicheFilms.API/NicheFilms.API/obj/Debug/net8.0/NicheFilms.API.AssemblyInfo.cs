4749572bc1b8f5ac588ea4fa65df872902c6d10b
RobertGardner13 <robbygardner26@gmail.com>
2025-04-07 11:19:53 -0600

    Port 5000

--- backend/NicheFilms.API/NicheFilms.API/obj/Debug/net8.0/NicheFilms.API.AssemblyInfo.cs
+++ backend/NicheFilms.API/NicheFilms.API/obj/Debug/net8.0/NicheFilms.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NicheFilms.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ec3e2f6eb1f068e949abc48e7dd074ceb8950f")]
 [assembly: System.Reflection.AssemblyProductAttribute("NicheFilms.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NicheFilms.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
