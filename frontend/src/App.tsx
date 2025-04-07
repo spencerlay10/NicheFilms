7b58ca43185b218a28ea0b6df554b1cbc557d723
Rex Tippetts <rex.tippetts@gmail.com>
2025-04-07 14:33:21 -0600

    Making home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,11 @@
 import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import CreateAccount from './pages/CreateAccount';
+// import Login from './pages/Login';
+// import Movie from './pages/Movie';
+// import Privacy from './pages/Privacy';
+// import ProductDetail from './pages/ProductDetail';
+
 
 
 function App() {
@@ -7,13 +14,13 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/createAccount" element={<CreateAccount />} />
+        <Route path="/" element={<Home/>} />
+        {/* <Route path="/createAccount" element={<CreateAccount />} />
         <Route path="login" element={<Login />} />
         <Route path="/movies" element={<Movie />} />
         <Route path="/privacy" element={<Privacy />} />
         <Route path="/productDetail" element={<ProductDetail />} />
-        <Route path="/createAccount" element={<CreateAccount />} />
+        <Route path="/createAccount" element={<CreateAccount />} /> */}
       </Routes>
 
     </div>
