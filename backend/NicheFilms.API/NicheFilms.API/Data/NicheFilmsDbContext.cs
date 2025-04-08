9d922cc47bc368dd8930b33a27f2c4e672ee6969
spdavis5 <spence.davis5@gmail.com>
2025-04-08 14:26:30 -0600

    Refactor database context configuration and remove sensitive connection strings

--- backend/NicheFilms.API/NicheFilms.API/Data/NicheFilmsDbContext.cs
+++ backend/NicheFilms.API/NicheFilms.API/Data/NicheFilmsDbContext.cs
@@ -26,11 +26,6 @@ public partial class NicheFilmsDbContext : DbContext
     public virtual DbSet<MoviesUser> MoviesUsers { get; set; }
 
     public virtual DbSet<UserRecommendation> UserRecommendations { get; set; }
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:nichefilms.database.windows.net,1433;Initial Catalog=NicheFilms;Persist Security Info=False;User ID=nicheadmin;Password=Gators2025!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<HybridRecommendation>(entity =>
