744c595e9b239f188cd828dca89c3fadd179e635
Rex Tippetts <rex.tippetts@gmail.com>
2025-04-09 10:33:58 -0600

    Fixing final localhost references

--- frontend/src/pages/CreateAccount.tsx
+++ frontend/src/pages/CreateAccount.tsx
@@ -30,7 +30,7 @@ const CreateAccount: React.FC = () => {
     } else {
       setError('');
       try {
-        const response = await fetch('https://localhost:5000/register', {
+        const response = await fetch('https://nichemovies-backend-byaza8g5hffjezf4.eastus-01.azurewebsites.net/register', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({ email, password }),
