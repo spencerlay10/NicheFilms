d59bf967c94b10f90e40f925f60c5849381ce1d2
spdavis5 <spence.davis5@gmail.com>
2025-04-10 12:44:58 -0600

    Comment out Header import in Admin component

--- frontend/src/pages/Admin.tsx
+++ frontend/src/pages/Admin.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Header from "../components/Header";
+// import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { API_BASE_URL } from "../api/config";
 
